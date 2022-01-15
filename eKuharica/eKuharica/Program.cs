14978c769c3897781fe4eff6a8690bfb2343b783
almahuskovic <alma.huskovic@edu.fit.ba>
2022-01-15 23:14:26 +0100

    docker

--- eKuharica/eKuharica/Program.cs
+++ eKuharica/eKuharica/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +14,15 @@ namespace eKuharica
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var database = scope.ServiceProvider.GetService<Context>();
+                new SetupService().Init(database);
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
