44b28cec3e9e04843f0aacb1dacad398bea9eb91
almahuskovic <alma.huskovic@edu.fit.ba>
2022-01-16 23:43:50 +0100

    starting with xamarin, login page

---
+++ eKuharica/eKuharica.Mobile/eKuharica.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using eKuharica.Mobile.Models;
+
+namespace eKuharica.Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ eKuharica/eKuharica.Mobile/eKuharica.Mobile/Views/LoginPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using eKuharica.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace eKuharica.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            //BindingContext = new LoginViewModel(); //moze se bindati i na ovaj nacin
+        }
+    }
+}
